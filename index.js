620f96959f292bbf3983d64b6ade54041230f06a
Kyle Anderson <46425993+kanderson425@users.noreply.github.com>
2019-05-23 08:43:33 -0500

    Adds isPrime function from codeWars

--- index.js
+++ index.js
@@ -228,6 +228,22 @@ function disemvowel(str) {
     return count;
   }
 
+  function isPrime(num) {
+    if (num<=1) {
+      return false;
+    } else if(num === 2) {
+      return true;
+    } else {
+      for(var x = 2; x < num; x++) {
+        if(num % x === 0) {
+          return false;
+        }
+      }
+      return true;  
+    }
+  }
+
+
 
 
 
